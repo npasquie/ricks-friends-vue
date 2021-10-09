d8aee1636ee4ae3c6d09b4151695b5665088ada6
t0bou <hyperstalker.z4@gmail.com>
2021-10-09 11:20:25 +0200

    initial

---
+++ src/components/TopBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <b-navbar variant="primary">
+    <b-navbar-brand class="title">
+      <img src="../assets/ricks-head.png" width="30" height="30" class="ricksHead"/>
+      Rick's Friends
+    </b-navbar-brand>
+  </b-navbar>
+</template>
+
+<script>
+  export default {
+    name: 'TopBar',
+    components: {}
+  }
+</script>
+
+<style scoped>
+  .title{
+    display: inline-block;
+    font-weight: bold;
+    color: white;
+  }
+
+  .ricksHead{
+    display: inline-block;
+    vertical-align: top;
+    margin-left: 1em;
+    margin-right: 1em;
+}
+</style>
---
+++ src/views/Characters.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="characters">
+    <top-bar/>
+    <p>felicitations bg</p>
+  </div>
+</template>
+
+<script>
+import TopBar from '@/components/TopBar.vue'
+
+export default {
+  name: 'Characters',
+  components: {
+    TopBar
+  }
+}
+</script>
