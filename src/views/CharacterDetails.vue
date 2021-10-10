8123e5317660d39726e9c9c7a3a8769d3f7d1d88
t0bou <hyperstalker.z4@gmail.com>
2021-10-10 21:14:24 +0200

    details page done

--- src/views/CharacterDetails.vue
+++ src/views/CharacterDetails.vue
@@ -1,17 +1,68 @@
 <template>
   <div class="characterDetails">
     <top-bar/>
-    hello
+    <b-container>
+      <b-spinner v-if="$apollo.loading"/>
+      <div v-if="!$apollo.loading">
+        <b-img center :src=character.image />
+        <h1>{{character.name}}</h1>
+        <p>{{character.species}} {{character.gender !== "unknown" ? character.gender : "of unknown gender"}}</p>
+        <p>Life Status : {{character.status}}</p>
+        <p>From {{character.origin.name}}</p>
+        <p v-if="character.origin.location != undefined">
+          Last seen in {{character.origin.location}}</p>
+        <p>Featured in :</p>
+        <ul>
+          <li v-for="episode in character.episode" :key=episode.name >
+            {{episode.name}}
+          </li>
+        </ul>
+      </div>
+    </b-container>
   </div>
 </template>
 
 <script>
 import TopBar from '@/components/TopBar.vue'
-//import { gql } from 'graphql-tag'
+import { gql } from 'graphql-tag'
 
 export default {
   name: 'CharacterDetails',
-  apollo:{},
+  apollo: {
+    character: {
+      query : gql`
+        query Character($id : ID!){
+          character(id: $id){
+            name,
+            species,
+            gender,
+            image,
+            status,
+            origin {
+              name
+            },
+            location {
+              name 
+            },
+            created,
+            episode {
+              name
+            }
+          }
+        }`,
+      variables() {
+        return {
+          id: this.$route.params.id
+        }
+      }
+    }
+  },
   components: { TopBar }
 }
 </script>
+
+<style scoped>
+  img{
+    margin: 1em;
+  }
+</style>
